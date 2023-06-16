6f1954eccee76474085840e2f00ee5f5ccc14994
Nkaka23dev <nkakaeric96@gmail.com>
2023-06-16 18:08:56 +0300

    feat: add exprss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
-const http = require("http");
+const app = require('./server')
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/') {
-        res.statusCode =  200;
-        console.log("HELLO THERE FROM THE SERVER")
-        res.end();
-    }
-})
-
-server.listen(3001, () => {
-    console.log("Server is live and running on the port: 3001")
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001")
 })
